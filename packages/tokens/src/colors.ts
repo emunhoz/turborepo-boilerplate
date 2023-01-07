19cfa9d4de8fca11f16e7e6ba012c0c04511cff9
Eder Munhoz dos Santos <emunhozdossantos@surveymonkey.com>
2023-01-07 02:56:35 -0300

    chore: updating token colors

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -11,10 +11,21 @@ export const colors = {
   gray800: '#202024',
   gray900: '#121214',
 
-  ignite300: '#00B37E',
-  ignite500: '#00875F',
-  ignite700: '#015F43',
-  ignite900: '#00291D',
+  myTokenColorName300: '#00B37E',
+  myTokenColorName500: '#00875F',
+  myTokenColorName700: '#015F43',
+  myTokenColorName900: '#00291D',
+
+  // Status
+  positive: '#117900',
+  negative: '#B90000',
+  warning: '#E69D00',
+
+  // Pastel
+  paleOyster: '#978F77',
+  mountainMist: '#8A888C',
+  nomad: '#B3B1A5',
+  swirl: '#D5D2C8',
 
   test: '#FFF',
 }
